a55cd2f7d21d0988eb29aadcc79b8551acf2a3df
Ivan <Korneychik91@yandex.ru>
2022-10-08 21:41:36 +0300

    first commit

---
+++ examples_18/Program.cs
@@ -0,0 +1,30 @@
+﻿static void quarter(int Quarter)
+{
+    if (Quarter == 1)
+        Console.WriteLine("x > 0 and y > 0");
+    else if (Quarter == 2)
+        Console.WriteLine("x < 0 and y > 0");
+    else if (Quarter == 3)
+        Console.WriteLine("x < 0 and y < 0");
+    else if (Quarter == 4)
+        Console.WriteLine("x > 0 and y < 0");
+    else Console.WriteLine("Не верно выбран номер четверти");
+}
+
+Console.Clear();
+
+int Quarter;
+
+try
+{
+    Console.WriteLine("Введи номер четверти, 1,2,3 или 4 ");
+    Quarter = int.Parse(Console.ReadLine() ?? "");
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных {exc.Message}");
+    return;
+}
+quarter(Quarter); 
+
+
---
+++ examples_21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу которая на вход принимает координаты двух точек и находит расстояние между ними в 2в пространстве
+Console.Clear();
+int x1, x2, y1, y2;
+
+static int koords(string message) 
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine() ?? "");
+    return result;
+}
+
+static double Distanse (int x1, int x2, int y1, int y2)
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return result;
+}
+
+
+
+x1 = koords("Введи координату X первой точки ");
+y1 = koords("Введи координату Y первой точки ");
+x2 = koords("Введи координату X второй точки ");
+y2 = koords("Введи координату Y второй точки ");
+
+double distanse = Distanse(x1, x2, y1, y2);
+Console.WriteLine(distanse);
+
+
+
