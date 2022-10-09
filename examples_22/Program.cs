ef05f868bb929008d0527cbeb164958907db7171
Ivan <Korneychik91@yandex.ru>
2022-10-09 13:07:31 +0300

    examples_22 done

---
+++ examples_22/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу которая на вход принимает число N и выдает таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+int usernum;
+
+try
+{
+    Console.Write("Введи целое число ");
+    usernum = int.Parse(Console.ReadLine() ?? "");
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"ошибка ввода данных {exc.Message} ");
+    return;
+}
+Console.Write($"{usernum} -> ");
+Pow(usernum);
+
+
+////////////////////////////////////////////////////////////////
+static void Pow(int usernum)
+{
+    usernum = Math.Abs(usernum);
+    for (int a = 1; a < usernum; a++)
+    {
+        Console.Write($"{a * a}, ");
+    }
+    Console.Write(usernum * usernum);
+}
+
+
